eccac9f1f9cb66dd886b7b970d9583f047a6c45d
Bo Xiao <boxiao.phys@gmail.com>
2026-04-28 23:07:58 -0400

    Cleaned up the main.jl file

--- src/main.jl
+++ src/main.jl
@@ -21,12 +21,15 @@ BLAS.set_num_threads(8)
 
 
 let
-    header = repeat("#", 200)
+    header = repeat("#", 70)
     println(header)
+    println("\nRunning DMRG simulation to obtain the ground-state wave function of the spin-2 model on a 1D chain")
     println(header)
-    println("\nStarting DMRG simulation to obtain the ground-state wave function of the spin-2 model on a 1D chain \n")
-    println("Parameters used in the simulation...")
+
     
+    # ---------------------------------------------------------------------------
+    # Parameters
+    # ---------------------------------------------------------------------------
     params = SimulationParameters(
         N       = 10,
         J₁      = 1.0,
@@ -36,47 +39,57 @@ let
         maxdim  = [20, 50, 200, 1000],
         seed    = 1234
     )
+    @info "Simulation parameters" params.N params.J₁ params.J₂ params.cutoff params.nsweeps params.maxdim params.seed
 
     
-    # Initialize a random MPS as the starting point of DMRG simulation
+
+    # ---------------------------------------------------------------------------
+    # Initial state
+    # ---------------------------------------------------------------------------
     Random.seed!(params.seed)
     sites = siteinds("S=2", params.N)
     ψ₀ = random_mps(sites, "+2"; linkdims=10)
     sz₀ = expect(ψ₀, "Sz"; sites=1:params.N)
+    @info "Initial ⟨Sᶻ⟩ profile" sz₀
+
+
     
-    println("\nInitial magnetization profile <Sz> at each site:")
-    @show sz₀
-
-
-    # Set up the Hamiltonian as an MPO
+    # ---------------------------------------------------------------------------
+    # Hamiltonian and DMRG
+    # ---------------------------------------------------------------------------
     Hamiltonian = build_hamiltonian(params, sites)  
     
 
     # Perform DMRG simulation to obtain the ground state
-    println("\nStarting DMRG simulation...")
+    println("\nStarting DMRG simulation...\n")
     E, ψ = dmrg(Hamiltonian, ψ₀; 
                 maxdim  = params.maxdim,
                 cutoff  = params.cutoff,
                 nsweeps = params.nsweeps
     )
-    println("")
-
-
-    sz  = expect(ψ, "Sz"; sites=1:params.N)
-    czz  = correlation_matrix(ψ, "Sz", "Sz"; sites=1:params.N)
-    println("\nFinal magnetization profile <Sz> at each site after DMRG:")
-    @show sz
     
 
+    
+    # ---------------------------------------------------------------------------
+    # Observables
+    # ---------------------------------------------------------------------------
+    sz  = expect(ψ, "Sz"; sites=1:params.N)
+    czz  = correlation_matrix(ψ, "Sz", "Sz"; sites=1:params.N)
+    
     # Check the variance of the energy
     H2 = inner(Hamiltonian, ψ, Hamiltonian, ψ)
     E₀ = inner(ψ', Hamiltonian, ψ)
     variance = H2 - E₀^2
-    println("\nVariance of the energy is $variance")
-    println(header)
-    println(header)
     
-
+    @printf("\nGround-state energy:        E = %.10f\n", E)
+    @printf("Energy variance:    ⟨H²⟩ − ⟨H⟩² = %.3e\n", variance)
+    @info "Final ⟨Sᶻ⟩ profile" sz       
+    
+   
+    
+    # ---------------------------------------------------------------------------
+    # Save results
+    # ---------------------------------------------------------------------------
     # output_filename = "data/heisenberg_input_n$(N).h5"
     # h5open(output_filename, "w") do file
     #     write(file, "Psi", ψ)
